e6fcb6706723573aeadb6abf8001eb079b86b901
Lara Maia <lara@craft.net.br>
2012-09-24 19:12:12 -0300

    New: Lista de pacotes

---
+++ packages/aur_packages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+yaourt --needed --noconfirm adeskbar adeskmenu aewan
+yaourt --needed --noconfirm agedu akonadi-kde-light alacarte-xfce
+yaourt --needed --noconfirm alsaequal asciiquarium bash-game-manager
+yaourt --needed --noconfirm caps cdspeed colibri
+yaourt --needed --noconfirm compton-git cpufrequtils cpuled
+yaourt --needed --noconfirm e4rat-lite-git eterm fltk11
+yaourt --needed --noconfirm freerapid freetype2-lcd geany-git
+yaourt --needed --noconfirm geany-plugins-git geany-themes-git glc-pulseaudio-git
+yaourt --needed --noconfirm gnome-colors-icon-theme gnome-colors-icon-theme-extras gnome-menus2
+yaourt --needed --noconfirm grub gtk-rezlooks-engine indigo
+yaourt --needed --noconfirm kcm-gtk kcm-qt-graphicssystem kde-gtk-config
+yaourt --needed --noconfirm kde-light kdebase-workspace-systemd kdepim-runtime-kde-light
+yaourt --needed --noconfirm kdeplasma-applet-keyleds kdeplasma-applets-simpleclock kdeplasma-applets-surtime
+yaourt --needed --noconfirm kdesudo ksh ksplash-theme-generator
+yaourt --needed --noconfirm lib32-alsaequal lib32-caps lib32-curl
+yaourt --needed --noconfirm lib32-glc-pulseaudio-git lib32-gtk-engine-murrine lib32-ladspa
+yaourt --needed --noconfirm lib32-libxxf86dga lib32-nvidia-utils-beta libast
+yaourt --needed --noconfirm libjpeg6 libsoprano-kde-light libstreamanalyzer-kde-light
+yaourt --needed --noconfirm libstreams-kde-light libtiff4 loic
+yaourt --needed --noconfirm mkinitcpio-numlock ncmatrix nepomuk-core-kde-light
+yaourt --needed --noconfirm nvidia-beta-zen oneko openssl098
+yaourt --needed --noconfirm package-query pacman-color pasteall
+yaourt --needed --noconfirm perl-weather-underground plymouth-git pulseaudio-equalizer
+yaourt --needed --noconfirm pydefrag-bzr python-pyalsaaudio qjoypad
+yaourt --needed --noconfirm r8168-zen rakarrack retext
+yaourt --needed --noconfirm screengrab sendmail sfxr
+yaourt --needed --noconfirm t50 tleds tomate
+yaourt --needed --noconfirm ttf-ms-fonts verynice volwheel-svn
+yaourt --needed --noconfirm weatherspect wine-mono winetricks-svn
+yaourt --needed --noconfirm xvidcap xxd yad
+yaourt --needed --noconfirm yafaray yaourt
---
+++ packages/update_catalog.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Autor: Lara Maia
+# Versão: 0.1
+
+aur_command="yaourt --needed --noconfirm"
+repo_command="pacman --needed --noconfirm"
+heap=3
+
+function showbar() {
+	local i=$(echo "($1 / $2) * 100" | bc -l)
+	local i=${i%%.*}; test $i || i=0
+
+	if [ $i -lt 10 ]; then
+		echo -ne "\e[42m \e[0m $i % \b\b\b\b\b"
+	fi
+	
+	if [ $i -ge 10 ]; then
+		echo -ne "\e[42m \e[0m $i % \b\b\b\b\b\b"
+	fi
+}
+
+packages_array_all=($(pacman -Qqe))
+packages_array_aur=($(pacman -Qqm))
+count_packages_all=${#packages_array_all[@]}
+
+echo -e "\nEscaneando\n"
+c=0; for package_local in ${packages_array_all[@]}; do
+	for package_aur in ${packages_array_aur[@]}; do
+		if [ $package_aur == $package_local ]; then
+			unset packages_array_all[$c]
+		fi
+	done
+	((c+=1))
+	showbar $c $count_packages_all
+done; echo -e '\n'
+
+count_packages_all=${#packages_array_all[@]}
+count_packages_aur=${#packages_array_aur[@]}
+
+echo -e "Gravando catalogo do aur\n"
+
+echo '#!/bin/bash' > aur_packages.sh
+c=0; i=0; for package in "${packages_array_aur[@]}"; do
+	if [ $i == 0 ]; then
+		echo -en "\n${aur_command} $package" >> aur_packages.sh
+		((i+=1))
+	elif [ $i -lt ${heap} ]; then
+		echo -n " $package" >> aur_packages.sh
+		((i+=1))
+	else
+		echo -ne "\n${aur_command} $package" >> aur_packages.sh
+		i=1
+	fi
+	((c+=1))
+	showbar $c $count_packages_aur
+done; echo -e '\n'
+
+echo -e "Gravando catalogo de repositórios\n"
+
+echo '#!/bin/bash' > repo_packages.sh
+c=0; i=0; for package in "${packages_array_all[@]}"; do
+	if [ $i == 0 ]; then
+		echo -en "\n${repo_command} $package" >> repo_packages.sh
+		((i+=1))
+	elif [ $i -lt ${heap} ]; then
+		echo -n " $package" >> repo_packages.sh
+		((i+=1))
+	else
+		echo -ne "\n${repo_command} $package" >> repo_packages.sh
+		i=1
+	fi
+	((c+=1))
+	showbar $c $count_packages_all
+done; echo
+
+echo -e "Operação Completa\n"
