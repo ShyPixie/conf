c6076ade87f8594d44d9a7227290af54c8f534dc
Lara Maia <lara@craft.net.br>
2013-10-14 17:50:40 -0300

    [update_catalog] Release 2.0, much faster

--- packages/update_catalog.sh
+++ packages/update_catalog.sh
@@ -1,80 +1,46 @@
 #!/bin/bash
 # Lara Maia © 2012 ~ 2013 <lara@craft.net.br>
 # Contributor: BlackICE <manfredi@gmail.com>
-# Versão: 1.3
+# Versão: 2.0
 
-echo -e "\nAguarde enquanto seu sistema é escaneado..."
+echo -e "\nAguarde enquanto seu sistema é escaneado...\n"
 
-packages_array_all=($(pacman -Qq))
-packages_array_aur=($(pacman -Qqm))
-count_packages_all=${#packages_array_all[@]}
-
-c=0; for package_local in ${packages_array_all[@]}; do
-	for package_aur in ${packages_array_aur[@]}; do
-		if [ $package_aur == $package_local ]; then
-			unset packages_array_all[$c]
-		fi
-	done
-	((c+=1))
-	
-	echo -ne "\e[1;32m==>\e[0m Analisando pacotes: "
-	echo -ne "\e[1;32m$c\e[0m de \e[1;32m$count_packages_all\e[0m \r"
-done; echo -e "\n"
-
-count_packages_all=${#packages_array_all[@]}
-count_packages_aur=${#packages_array_aur[@]}
+# Catalog files
 repo_file=("repo_packages_dep.cat" "repo_packages_exp.cat")
 aur_file=("aur_packages_dep.cat" "aur_packages_exp.cat")
 
-function checkpackage() {
-	local param=$(LANG=en_US pacman -Qi $1 | \
-	            grep Reason | awk '{print tolower($4)}')
-	            
-	if [ "$param" == "installed" ]; then
-		param='dependency'
-	fi
+function checkpackages() { # (list, 
+	#control variables
+	depends=0
+	explicit=0
 	
-	echo "$param"
+	for package in $1; do
+		if [ "$(pacman -Qqd $package)" == "$package" ]; then
+			eval file="\${${2}_file[0]}"
+			((depends+=1))
+		else
+			eval file="\${${2}_file[1]}"
+			((explicit+=1))
+		fi
+		
+		echo $package >> $file
+		
+		echo -ne "\e[1;32m==>\e[0m Gravando catálogo do $2: "
+		echo -ne "\e[1;32m$[$depends+$explicit]"
+		echo -ne "\e[0m de \e[1;32m$(echo "$1" | wc -w)\e[0m\r"
+	done
+	
+	
+	echo -ne "\n - \e[4;37mPacotes do $2\e[0m: "
+	echo -ne "\e[1;33m$explicit\e[0m instalados explicitamente, "
+	echo -e  "\e[1;33m$depends\e[0m dependências\n"
+
 }
 
-echo -n > ${aur_file[0]}; echo -n > ${aur_file[1]}
-c=0; d=0; e=0; for package in "${packages_array_aur[@]}"; do
-	type=$(checkpackage $package)
-	if [ $type == "dependency" ]; then
-		echo "$package" >> ${aur_file[0]}
-		((d+=1))
-	else
-		echo "$package" >> ${aur_file[1]}
-		((e+=1))
-	fi
-	((c+=1))
-	
-	echo -ne "\e[1;32m==>\e[0m Gravando catálogo do aur: "
-	echo -ne "\e[1;32m$c\e[0m de \e[1;32m$count_packages_aur\e[0m\r"
-done
+# Clean files before write
+rm -f ${aur_file[@]} ${repo_file[@]}
 
-echo -ne "\n - \e[4;37mPacotes do aur\e[0m: "
-echo -ne "\e[1;33m$e\e[0m instalados explicitamente, "
-echo -e  "\e[1;33m$d\e[0m dependências\n"
-
-echo -n > ${repo_file[0]}; echo -n > ${repo_file[1]}
-c=0; d=0; e=0; for package in "${packages_array_all[@]}"; do
-	type=$(checkpackage $package)
-	if [ $type == "dependency" ]; then
-		echo "$package" >> ${repo_file[0]}
-		((d+=1))
-	else
-		echo "$package" >> ${repo_file[1]}
-		((e+=1))
-	fi
-	((c+=1))
-	
-	echo -ne "\e[1;32m==>\e[0m Gravando catálogo dos repositórios: "
-	echo -ne "\e[1;32m$c\e[0m de \e[1;32m$count_packages_all\e[0m\r"
-done
-
-echo -ne "\n - \e[4;37mPacotes oficiais\e[0m: "
-echo -ne "\e[1;33m$e\e[0m instalados explicitamente, "
-echo -e  "\e[1;33m$d\e[0m dependências\n"
+checkpackages "$(pacman -Qqm)" aur
+checkpackages "$(pacman -Qqn)" repo
 
 echo -e "\e[1;32m==>\e[0m Operação concluída\n"
