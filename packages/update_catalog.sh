b0e16f089649553d8618d2d99fe911564924ff47
Lara Maia <lara@craft.net.br>
2013-11-08 15:15:38 -0200

    [update_catalog.sh] Speed Up

--- packages/update_catalog.sh
+++ packages/update_catalog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Lara Maia © 2012 ~ 2013 <lara@craft.net.br>
 # Contributor: BlackICE <manfredi@gmail.com>
-# Versão: 2.0
+# Versão: 3.0
 
 echo -e "\nAguarde enquanto seu sistema é escaneado...\n"
 
@@ -9,38 +9,34 @@ echo -e "\nAguarde enquanto seu sistema é escaneado...\n"
 repo_file=("repo_packages_dep.cat" "repo_packages_exp.cat")
 aur_file=("aur_packages_dep.cat" "aur_packages_exp.cat")
 
-function checkpackages() { # (list, 
-	#control variables
-	depends=0
-	explicit=0
-	
-	for package in $1; do
-		if [ "$(pacman -Qqd $package)" == "$package" ]; then
-			eval file="\${${2}_file[0]}"
-			((depends+=1))
-		else
-			eval file="\${${2}_file[1]}"
-			((explicit+=1))
-		fi
-		
-		echo $package >> $file
-		
-		echo -ne "\e[1;32m==>\e[0m Gravando catálogo do $2: "
-		echo -ne "\e[1;32m$[$depends+$explicit]"
-		echo -ne "\e[0m de \e[1;32m$(echo "$1" | wc -w)\e[0m\r"
-	done
-	
-	
-	echo -ne "\n - \e[4;37mPacotes do $2\e[0m: "
-	echo -ne "\e[1;33m$explicit\e[0m instalados explicitamente, "
-	echo -e  "\e[1;33m$depends\e[0m dependências\n"
+function ,() { echo -ne ".";}
 
+function checkpackages() { # (repository, package type
+	echo -ne "\e[1;32m==>\e[0m Gravando catálogo do repositório $1"
+	case "$1" in
+		aur)	 ,;pacman -Qqmd >> ${aur_file[0]};,
+				 ,;pacman -Qqme >> ${aur_file[1]};,
+				 ;;
+		oficial) ,;pacman -Qqnd >> ${repo_file[0]};,
+				 ,;pacman -Qqne >> ${repo_file[1]};,
+				 ;;
+	esac
+	
+	if [ "$1" == "aur" ]; then
+		packages_count=($(cat ${aur_file[0]} | wc -l) $(cat ${aur_file[1]} | wc -l))
+	else
+		packages_count=($(cat ${repo_file[0]} | wc -l) $(cat ${repo_file[1]} | wc -l))
+	fi
+	
+	echo -ne "\n - \e[4;37mPacotes do repositório $1\e[0m: "
+	echo -ne "\e[1;33m${packages_count[0]}\e[0m instalados explicitamente, "
+	echo -e  "\e[1;33m${packages_count[1]}\e[0m dependências\n"
 }
 
 # Clean files before write
 rm -f ${aur_file[@]} ${repo_file[@]}
 
-checkpackages "$(pacman -Qqm)" aur
-checkpackages "$(pacman -Qqn)" repo
+checkpackages aur
+checkpackages oficial
 
 echo -e "\e[1;32m==>\e[0m Operação concluída\n"
