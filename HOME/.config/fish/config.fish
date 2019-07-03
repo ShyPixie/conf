f26897a484ab2c1ff05921e87c5f77683b4b81fe
Lara Maia <dev@lara.click>
2019-07-03 11:57:27 -0300

    Fix lp command

--- HOME/.config/fish/config.fish
+++ HOME/.config/fish/config.fish
@@ -92,34 +92,33 @@ function exit; kill (pgrep -u $USER); end
 # ============== Useful functions ==========================
 
 function lp -d "ls with numerical permissions"
-    switch "$argv"
-        case '*l*'
-            echo -e (/bin/ls $argv -h --group-directories-first --indicator-style=classify | awk \
-              '{k=0
-                for(i=0; i<=8; i++) {
-                    k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i))
-                }
+    if not contains -- -l $argv
+        command ls $argv --color=always -h --group-directories-first --indicator-style=classify
+    else
+        echo -e (/bin/ls $argv -h --group-directories-first --indicator-style=classify | awk \
+          '{k=0
+            for(i=0; i<=8; i++) {
+                k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i))
+            }
 
-                if(k) {
-                    split($0, lsInfo, " ", sep)
-                    printf("\033[01;32m%0o \033[m", k)
-                    printf("\033[00;33m%s%s", lsInfo[1], sep[1])
-                    printf("\033[01;34m%s%s", lsInfo[2], sep[2])
-                    printf("\033[01;35m%s%s", lsInfo[3], sep[3])
-                    printf("\033[00;35m%s%s", lsInfo[4], sep[4])
-                    printf("\033[m%s%s", lsInfo[5], sep[5])
-                    printf("%s%s", lsInfo[6], sep[6])
-                    printf("%s%s", lsInfo[7], sep[7])
-                    printf("%s%s", lsInfo[8], sep[8])
-                    printf("\033[00;33m%s%s", lsInfo[9], sep[9])
-                    printf("%s%s", lsInfo[10], sep[10])
-                    printf("%s%s", lsInfo[11], sep[11])
-                    printf("\033[m\\\n")
-                }
-               }
-             ')
-        case "*"
-            command ls $argv --color=always -h --group-directories-first --indicator-style=classify
+            if(k) {
+                split($0, lsInfo, " ", sep)
+                printf("\033[01;32m%0o \033[m", k)
+                printf("\033[00;33m%s%s", lsInfo[1], sep[1])
+                printf("\033[01;34m%s%s", lsInfo[2], sep[2])
+                printf("\033[01;35m%s%s", lsInfo[3], sep[3])
+                printf("\033[00;35m%s%s", lsInfo[4], sep[4])
+                printf("\033[m%s%s", lsInfo[5], sep[5])
+                printf("%s%s", lsInfo[6], sep[6])
+                printf("%s%s", lsInfo[7], sep[7])
+                printf("%s%s", lsInfo[8], sep[8])
+                printf("\033[00;33m%s%s", lsInfo[9], sep[9])
+                printf("%s%s", lsInfo[10], sep[10])
+                printf("%s%s", lsInfo[11], sep[11])
+                printf("\033[m\\\n")
+            }
+           }
+         ')
     end
 end
 
